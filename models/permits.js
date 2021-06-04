29d1037a7ff764f62f8e7270d1ac3ddd3aa7355a
roblesisaac <irobles1030@gmail.com>
2021-06-04 12:05:20 -0700

    Update permits.js

--- models/permits.js
+++ models/permits.js
@@ -1,17 +1,38 @@
 const mongoose = require('mongoose');
+const rules = {
+    get: {
+      add: Array, // add this[propName] to query
+      remove: Array // removes this[propName] from query
+    },
+    put: {
+      add: Array,
+      remove: Array
+    },
+    post: {
+      add: Array,
+      remove: Array
+    },
+    delete: {
+      add: Array,
+      remove: Array
+    }
+}
 
 var permitSchema = new mongoose.Schema({
   username: String,
   sheetId: String,
   siteId: String,
   db: {
-    methods: Array
+    methods: Array,
+    rules: rules
   },
   ui: {
-    apps: Array
+    apps: Array,
+    rules: rules
   },
   permit: {
-    methods: Array
+    methods: Array,
+    rules: rules
   }
 });
 
