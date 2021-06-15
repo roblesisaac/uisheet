f3323f768524332fd65710723ef80312ce616ff3
roblesisaac <irobles1030@gmail.com>
2021-06-15 16:08:42 -0700

    Update sites.js

--- models/sites.js
+++ models/sites.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 var siteSchema = new mongoose.Schema({
   name: { type: String, unique: true },
+  clone: String,
   htmlButton: String,
   author: String,
   scripts: [
