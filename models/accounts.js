ef0f4f1b904b169c3c84d5f171f329d47acbf56b
roblesisaac <irobles1030@gmail.com>
2021-11-16 17:17:43 -0800

    Update accounts.js

--- models/accounts.js
+++ models/accounts.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const AccountSchema = new Schema({
-  userId: {
-    type: Schema.Types.ObjectId, 
-    ref: "users"
-  },
+  userIds: Array,
   accessToken: {
     type: String,
     required: true
