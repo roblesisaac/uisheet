2068ac22b233a645ff4a4f6e1aa08dadf2610abd
roblesisaac <irobles1030@gmail.com>
2021-11-18 12:08:13 -0800

    Update handler.js

--- handler.js
+++ handler.js
@@ -63,10 +63,7 @@ global.accounts = new Chain({
           
       models.accounts.findOne(filters, function(error, pAccount) {
         if(error) return self.error(error);
-        self.next({
-          data: pAccount,
-          boolean: !!pAccount
-        });
+        self.next(pAccount ? "true" : "false");
       });
     }
   },
