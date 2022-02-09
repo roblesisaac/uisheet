b55ca92dd1ab3779d4b430cd5cf4a23d87e38f66
roblesisaac <irobles1030@gmail.com>
2022-02-09 12:19:06 -0800

    Update handler.js

--- handler.js
+++ handler.js
@@ -1591,9 +1591,10 @@ global.ebay = new Chain({
       
       if(body.body) payload.body = payload.body;
       
-      nodeFetch(url, payload).then(res=>res.json()).then( data => {
-        this.next(data);
-      });
+      this.next(payload);
+      // nodeFetch(url, payload).then(res=>res.json()).then( data => {
+      //   this.next(data);
+      // });
     },
     generateUserAuthToken: function() {
       const scopes = [
